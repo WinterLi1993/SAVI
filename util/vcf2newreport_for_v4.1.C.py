30789d769eada7b0bb31cc7a7e8fa5bd5c1f7dfb
gitliver <oe2118@cumc.columbia.edu>
2015-05-11 17:20:56 -0400

    add snp.common, take max of cosmic_n.samples, move tot depth cols

--- util/vcf2newreport_for_v4.1.C.py
+++ util/vcf2newreport_for_v4.1.C.py
@@ -24,7 +24,7 @@ num_eff_fields = 11			# number of SnpEff fields delimited by "|" (change to 11 t
 emptyefflist = ['-']*num_eff_fields	# define empty eff list 
 
 # make a human readable version:
-header_readable = ['#chromosome', 'position', 'ref', 'alt', 'id.snp', 'snp_allele_freq', 'id.cosmic', 'cosmic_n.samples-mut']
+header_readable = ['#chromosome', 'position', 'ref', 'alt', 'id.snp', 'snp_allele_freq', 'snp.common', 'id.cosmic', 'cosmic_n.samples-mut', 'cosmic_n.samples-gene']
 
 # dict for entries in INFO field pertaining to SnpEff
 d_eff_info = { 	'EFFECTa.Effect_Impact': '-',
@@ -46,7 +46,8 @@ d_info = { 	'Sgt1MAXFREQ': '-',
 		'TOT_COUNTS': '-',
 		'SOM_VERIFIED_COUNTS': '-',
 		'CAF': '-',
-		'CNT': '-' } 
+		'CNT': '-',
+		'COMMON': '-' } 
 
 # dict to map vcf, SnpEff terms to human readable terms
 d_readable = {	'#CHROM':'#chromosome', 
@@ -77,14 +78,16 @@ d_readable = {	'#CHROM':'#chromosome',
 		'TOT_COUNTS':'cbio_total_mutation_count',
 		'dbSNPBuildID':'dbSNPBuild_for_RS',
 		'STRAND':'strand',
-		'CNT':'cosmic_n.samples-mut',
 		'CAF':'snp_allele_freq',
+		'CNT':'cosmic_n.samples-mut',
+		'COMMON': 'snp.common',
 		'Sgt1MAXFREQ':'Sgt1_max_frequency',
 		'S1ADPP':'S1_alt_depth_per_position' }
 
 # specify the format fields we want - only a subset of total:
 # ['GT', 'GQ', 'SDP', 'DP', 'RD', 'AD', 'FREQ', 'PVAL', 'RBQ', 'ABQ', 'RDF', 'RDR', 'ADF', 'ADR'] to
-format = ['SDP', 'RD', 'AD', 'RBQ', 'ABQ', 'RDF', 'RDR', 'ADF', 'ADR'] 
+# format = ['SDP', 'RD', 'AD', 'RBQ', 'ABQ', 'RDF', 'RDR', 'ADF', 'ADR'] 
+format = ['RD', 'AD', 'RBQ', 'ABQ', 'RDF', 'RDR', 'ADF', 'ADR'] 
 # make a human readable version:
 format_readable = [d_readable[j] for j in format]
 
@@ -188,9 +191,17 @@ for line in contents:
 			# print the non-savi INFO part of header
 			for myfield in sorted(d_info):
 
-				# make exceptions for CNT and CAF
-				if myfield in d_readable and myfield != 'CNT' and myfield != 'CAF': 
+				# make exceptions for CNT and CAF and COSMIC_NSAMP
+				if myfield in d_readable and myfield != 'CNT' and myfield != 'CAF' and myfield != 'COSMIC_NSAMP' and myfield != 'COMMON': 
 					print(d_readable[myfield] + "\t"),
+
+			# print SDP (FORMAT) part of header
+			for i in range(1,1+number_samples): 
+				# use sample names if user has provided them
+				if (args.samples):
+					print(d_readable['SDP'] + "_" + d_samples[str(i)] + "\t"),
+				else:
+					print(d_readable['SDP'] + "_" + str(i) + "\t"),
 
 			# print the savi INFO part of header (selective parts of the INFO field)
 			for myfield in sorted(d_savi_info):
@@ -252,6 +263,7 @@ for line in contents:
 		line_6 = ""
 		line_7 = ""
 		line_8 = ""
+		line_9 = ""
 
 		# split line on tab
 		linelist = line.split("\t")
@@ -322,12 +334,17 @@ for line in contents:
 
  		# print INFO part of line - sorted values
  		for myfield in sorted(d_info):
-			if myfield != 'CNT' and myfield != 'CAF': 
+			if myfield != 'CNT' and myfield != 'CAF' and myfield != 'COSMIC_NSAMP' and myfield != 'COMMON': 
  				line_6 += d_info[myfield] + "\t"
 
 		# CNT and CAF special cases
 		line_3 += d_info['CAF'] + "\t"
-		line_4 += d_info['CNT'] + "\t"
+		line_3 += d_info['COMMON'] + "\t"
+		# line_4 += d_info['CNT'] + "\t"
+		# line_4 += d_info['COSMIC_NSAMP'] + "\t"
+		# take max of comma delim list
+		line_4 += max(d_info['CNT'].split(",")) + "\t"
+		line_4 += max(d_info['COSMIC_NSAMP'].split(",")) + "\t"
 
  		# savi change string - this a composite of lower and upper bounds' vals 
  		savi_change = "nochange"
@@ -338,7 +355,7 @@ for line in contents:
 			# P1_F P2_F P3_F PD21_L PD21_U PD32_L PD32_U
 
 			if re.search(r'P(\d)_F', myfield):
- 				line_7 += d_savi_info[myfield] + "\t"
+ 				line_8 += d_savi_info[myfield] + "\t"
 			elif re.search(r'PD(\d+)_L', myfield):
 				if (d_savi_info[myfield] != "-"):
 					# lower bound > 0
@@ -350,7 +367,7 @@ for line in contents:
 				# upper bound < 0
 				elif int(d_savi_info[myfield]) < 0:
 					savi_change = "down"
- 				line_7 += savi_change + "\t"
+ 				line_8 += savi_change + "\t"
  				# reset
  				savi_change = "nochange"
 
@@ -360,7 +377,10 @@ for line in contents:
  			# ['GT', 'GQ', 'SDP', 'DP', 'RD', 'AD', 'FREQ', 'PVAL', 'RBQ', 'ABQ', 'RDF', 'RDR', 'ADF', 'ADR'] to
  			# ['SDP', 'RD', 'AD', 'RBQ', 'ABQ', 'RDF', 'RDR', 'ADF', 'ADR']
  			tmpformat = [v for j, v in enumerate(linelist[i].split(":")) if j not in [0,1,3,6,7]]
- 			line_8 += "\t".join(tmpformat) + "\t"
+			# isolate SDP column
+ 			line_7 += tmpformat[0] + "\t"
+			# join the rest
+			line_9 += "\t".join(tmpformat[1:]) + "\t"
 
 		# print out line
-		print(line_1 + line_2 + line_3 + line_4 + line_5 + line_6 + line_7 + line_8)
+		print(line_1 + line_2 + line_3 + line_4 + line_5 + line_6 + line_7 + line_8 + line_9)
